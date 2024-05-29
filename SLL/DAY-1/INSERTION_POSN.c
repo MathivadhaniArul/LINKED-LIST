679e3cbb7f77d6df78cfea3bc2a56f871bac9073
MathivadhaniArul <nilamathi312@gmail.com>
2024-05-29 07:31:32 +0530

    Create INSERTION_POSN.c

---
+++ SLL/DAY-1/INSERTION_POSN.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+int data;
+struct node *link;
+};
+
+void add(struct node* head,int data,int posn){
+struct node *ptr=head;
+struct node *a=malloc(sizeof(struct node));
+a->data=data;
+a->link=NULL;
+while(posn!=2){
+    ptr=ptr->link;
+    posn--;
+}
+a->link=ptr->link;
+ptr->link=a;
+
+};
+
+int main(){
+    struct node *head;
+    head=malloc(sizeof(struct node));
+    head->data=78;
+    head->link=NULL;
+
+    struct node *current;
+    current=malloc(sizeof(struct node));
+    current->data=69;
+    current->link=NULL;
+    head->link=current;
+
+    current=malloc(sizeof(struct node));
+    current->data=57;
+    current->link=NULL;
+    head->link->link=current;
+
+    add(head,54,3);
+return 0;
+}
