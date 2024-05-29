e166a30190c2f3c5b7879b3bb97aa76fabd4c823
MathivadhaniArul <nilamathi312@gmail.com>
2024-05-29 11:42:58 +0530

    Update DEL_LAST.c

--- SLL/DAY-2/DEL_LAST.c
+++ SLL/DAY-2/DEL_LAST.c
@@ -1,1 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+struct node{
+int data;
+struct node *link;
+};
+
+void del(struct node*head){
+ if(head->link==NULL){
+    free(head);
+    head=NULL;
+ }
+else{
+struct node *ptr=head;
+while(ptr->link->link!=NULL){
+    ptr=ptr->link;
+}
+ free(ptr->link);
+ ptr->link=NULL;
+ }
+}
+
+
+int main(){
+    struct node *head;
+    head=malloc(sizeof(struct node));
+    head->data=78;
+    head->link=NULL;
+
+    struct node *current;
+    current=malloc(sizeof(struct node));
+    current->data=69;
+    current->link=NULL;
+    head->link=current;
+
+    current=malloc(sizeof(struct node));
+    current->data=57;
+    current->link=NULL;
+    head->link->link=current;
+
+del(head);
+return 0;
+}
