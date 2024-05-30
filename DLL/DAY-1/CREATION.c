787828b7ac40875d71ae1ec201daa79f6886c5ad
MathivadhaniArul <nilamathi312@gmail.com>
2024-05-30 11:09:19 +0530

    Update CREATION.c

--- DLL/DAY-1/CREATION.c
+++ DLL/DAY-1/CREATION.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+typedef struct node {
+int data;
+struct node*prev;
+struct node*next;
+
+}Node;
+
+int main(){
+Node *head=malloc(sizeof(Node));
+head->prev=NULL;
+head->data=15;
+head->next=NULL;
+printf("%d ",head->data);
+
+return 0;
+}
