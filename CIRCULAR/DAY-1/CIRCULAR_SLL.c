d2b472cace2474f42d3e41577b17c28be7870da4
MathivadhaniArul <nilamathi312@gmail.com>
2024-05-31 10:32:06 +0530

    Update CIRCULAR_SLL.c

--- CIRCULAR/DAY-1/CIRCULAR_SLL.c
+++ CIRCULAR/DAY-1/CIRCULAR_SLL.c
@@ -1,1 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node {
+int data;
+struct node*next;
+}Node;
+
+Node* circular_ll(int n){
+Node* temp=malloc(sizeof(Node));
+temp->data=n;
+temp->next=temp;
+return temp;
+}
+
+int main(){
+Node* head=malloc(sizeof(Node));
+head=circular_ll(5);
+printf("%d ",head->data);
+return 0;
+}
 
